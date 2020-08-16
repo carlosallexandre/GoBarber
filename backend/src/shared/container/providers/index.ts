283ae2b76c2188e06016fcf7c7dc96f7862bd3c8
Carlos Rodrigues <carlos_costarodrigues@outlook.com>
2020-08-16 00:03:51 -0300

    feat: add aws ses

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,33 +1,4 @@
-import { container } from 'tsyringe';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import ICacheProvider from './CacheProvider/models/ICacheProvider';
-import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<ICacheProvider>(
-  'CacheProvider',
-  RedisCacheProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  container.resolve(EtherealMailProvider),
-);
+import './CacheProvider';
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
